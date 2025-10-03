966c9085d0f1a04f69dc5ff6bcb79353cb15feb0
Newwiens <ht.nguyen013@gmail.com>
2025-10-03 10:38:50 +0200

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,10 @@ async function loadData() {
   try {
     // STAP 1: Probeer data op te halen
     /* ----- Voor Vercel deploy ------*/
-    // const response = await fetch("public/data/data.json");
+    const response = await fetch("/data/data.json");
 
     /* --------- Local host ---------- */
-    const response = await fetch("public/data/data.json");
+    //const response = await fetch("public/data/data.json");
 
     // STAP 2: Check of response OK is
     if (!response.ok) {
