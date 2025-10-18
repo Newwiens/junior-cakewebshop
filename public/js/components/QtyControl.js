8b33386e46566b471b1cfd9b9a04caa2d6696db3
Newwiens <ht.nguyen013@gmail.com>
2025-10-18 12:35:13 +0200

    qcontrol updated

--- public/js/components/QtyControl.js
+++ public/js/components/QtyControl.js
@@ -13,9 +13,32 @@ export function initQtyControl() {
     const clickedBtn = event.target.closest(".qty-btn");
     if (!clickedBtn) return; //--> //Als niet klikt, stoppen
 
-    // 3b. Lees type actie btn voor console.log (data-action = "inc" of "dec")
+    // 3b. Zoek naar ouder div waar de qty-btn in zitten
+    const controls = clickedBtn.closest(".qty-controls");
+    if (!controls) return;
+
+    // 3c. Zoek het element waar de waarde wordt weergegeven
+    const output = controls.querySelector(".qty-value");
+    if (!output) return;
+
+    // 3d. Lees huidige waarde e nzet om naar getal
+    // parseInt = string omzet naar getal
+    let value = parseInt(output.textContent, 10); // --> JS lees menselijk getal 0-9 (base 10)
+    if (Number.isNaN(value)) value = 0; //-->
+
+    // 3e. Lees type actie btn voor console.log (data-action = "inc" of "dec")
     const action = clickedBtn.dataset.action;
 
-    console.log("Knop geklikt:", action);
+    // 3f. Pas waarde aan
+    if (action === "inc") {
+      value += 1;
+    } else if (action === "dec") {
+      value = Math.max(0, value - 1);
+    }
+    // 3g. Schrijf nieuwe waarde terug naar UI
+    output.textContent = value;
+
+    // 3h. Voor test
+    console.log("Nieuwe waarde:", value);
   });
 }
